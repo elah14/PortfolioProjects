b69750245da2228ea2625d1fbad883f0a2c65383
elah14 <103892043+elah14@users.noreply.github.com>
2026-03-09 23:10:38 +0100

    Nettoyage de données avec SQL
    Gestion et nettoyage de données liées a l´immobilier américain grâce a SQL SERVER

---
+++ Nettoyage de données.sql
@@ -0,0 +1,143 @@
+/*
+Nettoyage des données dans les requêtes SQL
+*/
+
+-- Aperçu initial des données
+Select *
+From PortfolioProject.dbo.NashvilleHousing
+
+--------------------------------------------------------------------------------------------------------------------------
+
+-- Standardiser le format de la date (conversion vers le type Date)
+
+Select saleDateConverted, CONVERT(Date,SaleDate)
+From PortfolioProject.dbo.NashvilleHousing
+
+Update NashvilleHousing
+SET SaleDate = CONVERT(Date,SaleDate)
+
+-- Si la mise à jour directe ne fonctionne pas, nous ajoutons une nouvelle colonne
+ALTER TABLE NashvilleHousing
+Add SaleDateConverted Date;
+
+Update NashvilleHousing
+SET SaleDateConverted = CONVERT(Date,SaleDate)
+
+
+ --------------------------------------------------------------------------------------------------------------------------
+
+-- Remplir les données d'adresse de propriété manquantes (NULL)
+-- On utilise le ParcelID pour retrouver l'adresse associée à une même parcelle
+
+Select *
+From PortfolioProject.dbo.NashvilleHousing
+order by ParcelID
+
+Select a.ParcelID, a.PropertyAddress, b.ParcelID, b.PropertyAddress, ISNULL(a.PropertyAddress,b.PropertyAddress)
+From PortfolioProject.dbo.NashvilleHousing a
+JOIN PortfolioProject.dbo.NashvilleHousing b
+	on a.ParcelID = b.ParcelID
+	AND a.[UniqueID ] <> b.[UniqueID ]
+Where a.PropertyAddress is null
+
+Update a
+SET PropertyAddress = ISNULL(a.PropertyAddress,b.PropertyAddress)
+From PortfolioProject.dbo.NashvilleHousing a
+JOIN PortfolioProject.dbo.NashvilleHousing b
+	on a.ParcelID = b.ParcelID
+	AND a.[UniqueID ] <> b.[UniqueID ]
+Where a.PropertyAddress is null
+
+--------------------------------------------------------------------------------------------------------------------------
+
+-- Diviser l'adresse en colonnes individuelles (Adresse, Ville, État)
+-- Utilisation de SUBSTRING et CHARINDEX pour la colonne PropertyAddress
+
+SELECT
+SUBSTRING(PropertyAddress, 1, CHARINDEX(',', PropertyAddress) -1 ) as Address
+, SUBSTRING(PropertyAddress, CHARINDEX(',', PropertyAddress) + 1 , LEN(PropertyAddress)) as City
+From PortfolioProject.dbo.NashvilleHousing
+
+ALTER TABLE NashvilleHousing
+Add PropertySplitAddress Nvarchar(255);
+
+Update NashvilleHousing
+SET PropertySplitAddress = SUBSTRING(PropertyAddress, 1, CHARINDEX(',', PropertyAddress) -1 )
+
+ALTER TABLE NashvilleHousing
+Add PropertySplitCity Nvarchar(255);
+
+Update NashvilleHousing
+SET PropertySplitCity = SUBSTRING(PropertyAddress, CHARINDEX(',', PropertyAddress) + 1 , LEN(PropertyAddress))
+
+-- Utilisation de PARSENAME pour la colonne OwnerAddress (plus simple que SUBSTRING pour les délimiteurs multiples)
+
+Select
+PARSENAME(REPLACE(OwnerAddress, ',', '.') , 3)
+,PARSENAME(REPLACE(OwnerAddress, ',', '.') , 2)
+,PARSENAME(REPLACE(OwnerAddress, ',', '.') , 1)
+From PortfolioProject.dbo.NashvilleHousing
+
+ALTER TABLE NashvilleHousing
+Add OwnerSplitAddress Nvarchar(255);
+
+Update NashvilleHousing
+SET OwnerSplitAddress = PARSENAME(REPLACE(OwnerAddress, ',', '.') , 3)
+
+ALTER TABLE NashvilleHousing
+Add OwnerSplitCity Nvarchar(255);
+
+Update NashvilleHousing
+SET OwnerSplitCity = PARSENAME(REPLACE(OwnerAddress, ',', '.') , 2)
+
+ALTER TABLE NashvilleHousing
+Add OwnerSplitState Nvarchar(255);
+
+Update NashvilleHousing
+SET OwnerSplitState = PARSENAME(REPLACE(OwnerAddress, ',', '.') , 1)
+
+--------------------------------------------------------------------------------------------------------------------------
+
+-- Harmoniser le champ "Sold as Vacant" : remplacer Y/N par Yes/No
+
+Select Distinct(SoldAsVacant), Count(SoldAsVacant)
+From PortfolioProject.dbo.NashvilleHousing
+Group by SoldAsVacant
+order by 2
+
+Update NashvilleHousing
+SET SoldAsVacant = CASE When SoldAsVacant = 'Y' THEN 'Yes'
+	   When SoldAsVacant = 'N' THEN 'No'
+	   ELSE SoldAsVacant
+	   END
+
+-----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+-- Supprimer les doublons
+-- Utilisation d'une CTE et de ROW_NUMBER pour identifier les lignes identiques
+
+WITH RowNumCTE AS(
+Select *,
+	ROW_NUMBER() OVER (
+	PARTITION BY ParcelID,
+				 PropertyAddress,
+				 SalePrice,
+				 SaleDate,
+				 LegalReference
+				 ORDER BY
+					UniqueID
+					) row_num
+From PortfolioProject.dbo.NashvilleHousing
+)
+-- On sélectionne ici les doublons (row_num > 1). Remplacer "Select *" par "DELETE" pour les supprimer.
+Select *
+From RowNumCTE
+Where row_num > 1
+Order by PropertyAddress
+
+---------------------------------------------------------------------------------------------------------
+
+-- Supprimer les colonnes inutilisées (Nettoyage final après transformation)
+
+ALTER TABLE PortfolioProject.dbo.NashvilleHousing
+DROP COLUMN OwnerAddress, TaxDistrict, PropertyAddress, SaleDate
